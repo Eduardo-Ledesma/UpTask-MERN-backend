e3744ac709e5d69678434f0c5706823fb09a67f2
Eduardo Ledesma <e.ledesma.higonet@gmail.com>
2023-09-15 18:48:21 -0300

    added url

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 dotenv.config();
 // Configure CORS
-const whitelist = [process.env.FRONTEND_URL];
+const whitelist = ['https://uptask-lac.vercel.app/'];
 const corsOptions = {
     origin: function(origin, callback) {
         if(whitelist.includes(origin)) {
